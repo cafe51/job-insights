c248cfa908d55a7ac553277bc10dd9d7f031b310
cafe51 <cafecafe51@hotmail.com>
2023-03-14 00:04:23 -0400

    requisito 2 resolvido

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -38,7 +38,7 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    return list(set([element['job_type'] for element in read(path)]))
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
