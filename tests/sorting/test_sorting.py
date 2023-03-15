079fa0178c5d61204057470e3fe312453533797b
cafe51 <cafecafe51@hotmail.com>
2023-03-14 22:41:32 -0400

    requisito 11 resolvido

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    results = ['383416', '19857', "2020-05-08"]
+    criteria = ["max_salary", "min_salary", "date_posted"]
+    jobs = read('data/jobs.csv')
+
+    for index in range(len(criteria)):
+        sort_by(jobs, criteria[index])
+        assert jobs[0][criteria[index]] == results[index]
+
+
